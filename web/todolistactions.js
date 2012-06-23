3bbb214097b0501210d65d2f14e89370f9fd8215
Anna Bolotova <ann.bolotova@gmail.com>
2012-06-23 18:59:39 +0400

    initial

---
+++ web/todolistactions.js
@@ -0,0 +1,26 @@
+function init() {
+    var container = document.getElementById("days-container");
+    var divs = container.getElementsByTagName('div');
+    for (i in divs) {
+        var textField = divs[i].getElementsByTagName('input')[0];
+        var list = divs[i].getElementsByTagName('ul')[0];
+        textField.onkeydown = createTypingHandler(textField, list)
+    }
+}
+
+function createTypingHandler(textField, list) {
+    return function(e) {
+        if (typeof e == 'undefined' && window.event) {
+            e = window.event;
+        }
+        if (e.keyCode == 13) {
+
+            var text = textField.value
+
+            var newElement = document.createElement("li");
+            newElement.innerHTML = text
+            list.appendChild(newElement)
+            textField.value = ''
+        }
+    }
+}
