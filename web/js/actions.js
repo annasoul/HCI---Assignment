afb6494edc5f9fe9e7ed13498a5cb631169e915a
Anna Bolotova <ann.bolotova@gmail.com>
2012-06-25 21:25:36 +0400

    initial task dnd support is added

--- web/js/actions.js
+++ web/js/actions.js
@@ -1,55 +1,84 @@
 $(document).ready(function () {
     initTasks();
+    initDragAndDrop();
     initCalendar();
     initDates();
 });
 
+function addTask(text, list) {
+    var added = false;
+    var htmlToSet = '<input type="checkbox">' + text +'<br>';
+    var tasks = $('div.task', list).each(function () {
+        if (added) {
+            return;
+        }
+        if (this.innerHTML == '') {
+            this.innerHTML = htmlToSet;
+            added = true;
+        }
+    });
+
+    if (!added) {
+        $('<div class="task">' + htmlToSet + '</div>').appendTo(list);
+    }
+}
+
+function initDragAndDrop() {
+    var findList = function(startChild) {
+        for (var e = startChild; e; e = e.parentElement) {
+            if ($(e).hasClass('list')) {
+                return e;
+            }
+        }
+        return null;
+    };
+
+    var parseListId = function(e) {
+        var classes = e.className.split(' ');
+        for (var i in classes) {
+            if (classes[i].indexOf('myListId') == 0) {
+                return classes[i]
+            }
+        }
+        return null;
+    };
+
+    $('div.day div.list').each(function (i) {
+        var list = $(this);
+        var listId = parseListId(this);
+        list.droppable({
+            accept: 'div.task:not(.' + listId + ')',
+            drop: function(event, ui) {
+                var list = findList(event.target);
+                if (!list) {
+                    return;
+                }
+                addTask($(event.toElement).text(), list);
+                $('.ui-draggable-dragging').hide();
+            }
+        });
+    })
+}
+
 function initTasks() {
-    $('div.day').each(function (i) {
+    $('div.day').each(function (listId) {
         var textField = $('input', this);
         var list = $('div.list', this);
-
-        // Configure drop targets.
-        $('div.list').each(function (i, list) {
-            $(list).droppable({
-                accept: 'div.task',
-                drop: function(event, ui) {
-                    if (true) {
-                        $('.ui-draggable-dragging').hide();
-                    }
-                }
-            })
-        });
+        $(list).addClass('myListId' + listId);
 
         // Add empty cells.
         for (var j = 0; j < 10; j++) {
             var task = $('<div class="task"></div>');
+            task.addClass('myListId' + listId)
             task.draggable({ revert: "invalid" });
             task.appendTo(list);
         }
 
         // Setup typing handler.
         textField.bind('keypress', function (e) {
-            var innerHtml = '<input type="checkbox">' + textField.val() +'<br>';
-
             if (e.keyCode == 13 && textField.val() != '') {
-                var added = false;
-                var tasks = $('div.task', list).each(function () {
-                    if (added) {
-                        return;
-                    }
-                    if (this.innerHTML == '') {
-                        this.innerHTML = innerHtml;
-                        added = true;
-                    }
-                });
-
-                if (!added) {
-                    $('<div class="task">' + innerHtml + '</div>').appendTo(list);
-                }
-
-                //clear text field
-                textField.val('')
+                addTask(textField.val(), list);
+                textField.val('');
             }
         });
     })
